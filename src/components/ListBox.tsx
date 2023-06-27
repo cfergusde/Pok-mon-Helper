39dac2c0172aa3e3bcbedac7166bd2192bc78b9a
sdavisde <sdavisde@gmail.com>
2023-06-27 16:58:23 -0500

    updates to styles

--- src/components/ListBox.tsx
+++ src/components/ListBox.tsx
@@ -1,15 +1,18 @@
-'use client';
+"use client";
 
 type ListBoxProps = {
-  index: number,
-  click?: () => void,
-  children: any
-}
+  index: number;
+  click?: () => void;
+  children: any;
+};
 
 export const ListBox = ({ index, click, children }: ListBoxProps) => {
   return (
-    <div onClick={click} className="border-2 border-slate-950 w-40 h-12">
-      {children}
+    <div onClick={click} className="border-slate-950 w-40 h-18 flex">
+      <div className="w-12 h-full border flex justify-center items-center bg-gray-300">
+        <h2>{index}</h2>
+      </div>
+      <div className="w-28 flex justify-center items-center">{children}</div>
     </div>
-  )
-}
+  );
+};
