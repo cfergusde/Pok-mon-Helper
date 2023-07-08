7742d7d54d4842825805a8c32b4756601a37e52e
Connor Fergus <connorpfergus@yahoo.com>
2023-07-07 19:03:43 -0500

    UpdatedLandingPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import PlatinumImg from '../assets/platinum-logo-large.jpg'
-
+import Black2Img from '../assets/Black2Logo.png'
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-between items-center p-24 min-h-screen">
-      <p>
-        Welcome to Pokemon helper. Click the game you want to use:
+      <p className="bg-amber-300 text-sky-700 font-mono font-bold text-4xl text-black p-2 border-x-4 border-sky-700">
+        Welcome to Pokéhelper! Click the game you want to use below:
       </p>
-      <div className="">
+      <div className="border-solid border-black border-2 hover:border-dotted">
         <Link href={'./start'}>
             <Image 
                 src={PlatinumImg}
@@ -17,6 +17,16 @@ export default function Home() {
                 width={200}
                 height={100}
             />
+        </Link>
+      </div>
+      <div className="border-solid border-black border-2 hover:border-dotted p-2">
+        <Link href={'./start'}>
+          <Image
+            src={Black2Img}
+            alt="Pokemon Black 2 Img"
+            width={200}
+            height={100}
+          />
         </Link>
       </div>
     </main>
