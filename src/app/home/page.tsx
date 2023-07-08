8cf2361a4961f3e4d6985576d0d4cb997a123704
sdavisde <sdavisde@gmail.com>
2023-07-07 22:12:17 -0500

    added timeline

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -12,6 +12,9 @@ export default function Home() {
         <div className="outline outline-black w-1/3 h-full centered">Items</div>
         <div className="outline outline-black w-1/3 h-full centered">Other</div>
       </div>
+      <div className="outline outline-black w-full h-96 centered">
+        Timeline Section
+      </div>
     </main>
   );
 }
